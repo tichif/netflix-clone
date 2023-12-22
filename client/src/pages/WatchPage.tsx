206e6f7a11a80056c3e732ebd4e204b70afb7e64
Dalzon Charles-Hébert <charlby5@gmail.com>
2023-12-21 22:51:10 -0500

    Show video based on the id

--- client/src/pages/WatchPage.tsx
+++ client/src/pages/WatchPage.tsx
@@ -1,16 +1,35 @@
+import { useParams, useNavigate } from 'react-router-dom';
+
 import { ArrowLeftIcon } from '@heroicons/react/24/outline';
+import useMovie from '../hooks/useMovie';
 
 const WatchPage = () => {
+  const params = useParams() as { id: string };
+  const navigate = useNavigate();
+
+  const { data, error, loading } = useMovie(params.id);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error || !data) {
+    return <p>{error}</p>;
+  }
+
   return (
     <div className='h-screen w-screen bg-black'>
       <nav className='fixed w-full z-10 flex items-center gap-8 p-4 bg-black bg-opacity-80'>
-        <ArrowLeftIcon className='w-10 text-white cursor-pointer hover:opacity-80 transition' />
+        <ArrowLeftIcon
+          className='w-10 text-white cursor-pointer hover:opacity-80 transition'
+          onClick={() => navigate('/browse')}
+        />
         <p className='text-white text-3xl font-bold'>
-          <span className='font-light'>Watching:</span> South Park
+          <span className='font-light'>Watching:</span> {data.title}
         </p>
       </nav>
       <iframe
-        src='https://www.youtube.com/embed/m8nKhZY--0g?si=N1TMVGftedW8rR-O?autoplay=1'
+        src={data.videoUrl}
         className='h-full w-full'
         allowFullScreen
         allow='autoplay'
--- server/index.js
+++ server/index.js
@@ -13,6 +13,13 @@ app.get('/movies/list', (req, res) => {
   return res.send(movies);
 });
 
+app.get('/movie/:id', (req, res) => {
+  const { id } = req.params;
+  const movie = movies.find((movie) => movie.id === id);
+
+  return res.send(movie);
+});
+
 app.listen(PORT, () => {
   console.log(`App is running on port ${PORT}`);
 });
